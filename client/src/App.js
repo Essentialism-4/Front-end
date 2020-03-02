f86e3bba6fc52a8eec0762bd7dba4f1772247eb2
Issac Moreno <issaxavier909@gmail.com>
2020-03-02 12:58:26 -0800

    Folder structure day 1

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,47 @@
-import React from 'react';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+import PrivateRoute from "./components/PrivateRoute";
+import UserValues from "./components/UserValues";
+
+import { axiosWithAuth } from "./utils/axiosWithAuth";
+
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
+  const token = localStorage.getItem("token");
+  const [loggedIn, setLoggedIn] = useState(token ? true : false);
 
-    </div>
+  const register = payload => {
+    axiosWithAuth()
+      .post("api/auth/register", payload)
+      .then(res => {
+        console.log(res);
+        localStorage.setItem("token", res.data.token);
+      })
+      .catch(err => console.log(err));
+  };
+
+  const login = credentials => {
+    axiosWithAuth()
+      .post("api/auth/login", credentials)
+      .then(res => {
+        console.log(res)
+      })
+      .catch(err => console.log(err));
+  };
+
+  useEffect(() => {
+    setLoggedIn(loggedIn === false ? false : true);
+  }, []);
+
+  return (
+    <Router>
+      <div className="App">
+        <Route path = '/' />
+          <PrivateRoute exact path = '/' isLoggedin = {loggedIn}  />
+      </div>
+    </Router>
   );
 }
 
---
+++ client/src/components/ValueList.js
@@ -0,0 +1,10 @@
+import React from 'react';
+{/* THIS COMPONENT WILL BE SHOWN TO A NEW USER. IT WILL CONTAIN A LIST OF VALUES FOR THE USER TO SELECT FROM */}
+
+const ValueList = () => {
+    return (
+        <></>
+    )
+}
+
+export default ValueList;
